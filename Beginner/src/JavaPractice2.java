3a42cd37a71c3a615b47713faadfe8cda2064366
nguyenle <nguyenle@nguyens-macbook-pro.home>
2020-09-18 12:14:03 +0200

    Added task 5 and 7

--- Beginner/src/JavaPractice2.java
+++ Beginner/src/JavaPractice2.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class JavaPractice2
 {
-    public static int amount;
-    public static int amount1;
+    public static int amount3;
+    public static int amount4;
+    public static int amount5;
+    public static int terms;
     public static void main(String[]args)
     {
         Scanner scan = new Scanner(System.in);
@@ -33,16 +35,16 @@ public class JavaPractice2
         //*****
         //Write a program where the user can enter a number and a box of that size will be generated.
         System.out.println("Enter amount: ");
-        String am = scan.nextLine();
-        boolean statusAmount = false;
-        while(statusAmount==false){
+        String am3 = scan.nextLine();
+        boolean statusAmount3 = false;
+        while(statusAmount3==false){
             try{
-                amount = Integer.parseInt(am);
-                statusAmount = true;
-            }catch (NumberFormatException amount) {
-                statusAmount=false;
+                amount3 = Integer.parseInt(am3);
+                statusAmount3 = true;
+            }catch (NumberFormatException amount3) {
+                statusAmount3=false;
                 System.out.println("Please input integer value: ");
-                am = scan.nextLine();
+                am3 = scan.nextLine();
             }
         }
         //Outer loop to repeat that inside loop amount times, each time go new line
@@ -54,11 +56,11 @@ public class JavaPractice2
         //x
         //x
         //x
-        for(int i = 0; i < amount; i++)
+        for(int i = 0; i < amount3; i++)
         {
             //Inside loop to print * amount times horizontally
             //E.g. amount = 5 --> this loop prints *****
-            for(int j = 0; j < amount; j++)
+            for(int j = 0; j < amount3; j++)
             {
                 System.out.print("*");
             }
@@ -72,18 +74,68 @@ public class JavaPractice2
         //*   *
         //*****
         System.out.println("Enter amount:");
-        String am1 = scan.nextLine();
-        boolean statusAmount1 = false;
-        while(statusAmount1==false){
+        String am4 = scan.nextLine();
+        boolean statusAmount4 = false;
+        while(statusAmount4==false){
             try{
-                amount1 = Integer.parseInt(am1);
-                statusAmount1 = true;
-            }catch (NumberFormatException amount1) {
-                statusAmount1=false;
+                amount4 = Integer.parseInt(am4);
+                statusAmount4 = true;
+            }catch (NumberFormatException amount4) {
+                statusAmount4=false;
                 System.out.println("Please input integer value: ");
-                am1 = scan.nextLine();
+                am4 = scan.nextLine();
             }
         }
-        
+
+        //Task 5 --> Create a program which creates a right angled triangle using asterisks.
+        //The user should choose the triangle’s size. As an example, a triangle of size 5 looks like this:
+        //*
+        //**
+        //***
+        //****
+        //*****
+        System.out.println("Enter amount: ");
+        String am5 = scan.nextLine();
+        boolean statusAmount5 = false;
+        while(statusAmount5==false){
+            try{
+                amount5 = Integer.parseInt(am5);
+                statusAmount5 = true;
+            }catch (NumberFormatException amount5) {
+                statusAmount5=false;
+                System.out.println("Please input integer value: ");
+                am5 = scan.nextLine();
+            }
+        }
+        for(int i = 0; i < amount5; i++)
+        {
+            for(int j = 0; j < i+1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        //Task 7 --> A number sequence follows the rule: double, then add 3. It starts with the numbers: 1, 5, 13, 29, 61.
+        //What is the 40th number in the sequence?
+        System.out.println("Enter nth term: ");
+        String term = scan.nextLine();
+        boolean statusTerm = false;
+        while(statusTerm==false){
+            try{
+                terms = Integer.parseInt(term);
+                statusTerm = true;
+            }catch (NumberFormatException terms) {
+                statusTerm=false;
+                System.out.println("Please input integer value: ");
+                term = scan.nextLine();
+            }
+        }
+        int current = 1;
+        for(int i = 0; i < terms; i++)
+        {
+            current = current * 2 + 3;
+        }
+        System.out.println(current);
     }
 }
